3349d78c3ee46405e9c3a9d9d527b0cf476499e5
Muhammad Ahmad <muhammadahmad788454@gmail.com>
2024-08-19 21:00:41 +0500

    task07_phase1_completed

---
+++ task_7_bitebuddy/biteBuddy/lib/screens/home/home_screen.dart
@@ -0,0 +1,207 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/config/colors.dart';
+import 'package:food_app/providers/user_provider.dart';
+import 'package:provider/provider.dart';
+import 'drawer_side.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  Widget _buildClassicComfortsProduct(context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Classic Comforts'),
+              GestureDetector(
+                child: Text(
+                  'view all',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildCrispyCravingsProduct(context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Crispy Cravings'),
+              GestureDetector(
+                child: Text(
+                  'view all',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildFreshBitesProduct() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Fresh Bites'),
+              GestureDetector(
+                child: Text(
+                  'view all',
+                  style: TextStyle(color: Colors.grey),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    UserProvider userProvider = Provider.of(context);
+    userProvider.getUserData();
+    return Scaffold(
+      drawer: DrawerSide(
+        userProvider: userProvider,
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        iconTheme: IconThemeData(color: textColor),
+        title: Text(
+          'Home',
+          style: TextStyle(color: textColor, fontSize: 17),
+        ),
+        actions: [
+          CircleAvatar(
+            radius: 15,
+            backgroundColor: Colors.white,
+            child: Icon(
+              Icons.search,
+              size: 21,
+              color: textColor,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 5),
+            child: GestureDetector(
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 15,
+                child: Icon(
+                  Icons.shop,
+                  size: 21,
+                  color: textColor,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+        child: ListView(
+          children: [
+            Container(
+              height: 150,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/banner.jpeg'),
+                ),
+                color: Colors.teal,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(right: 130, bottom: 10),
+                            child: Container(
+                              height: 52,
+                              width: 100,
+                              decoration: BoxDecoration(
+                                color: Colors.teal,
+                                borderRadius: BorderRadius.only(
+                                  bottomRight: Radius.circular(50),
+                                  bottomLeft: Radius.circular(50),
+                                ),
+                              ),
+                              child: Center(
+                                child: Text(
+                                  'Discount',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Text(
+                            '30% Off',
+                            style: TextStyle(
+                                fontSize: 40,
+                                color: Colors.green[100],
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 20),
+                            child: Text(
+                              'On all the Fresh Bites Products',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(),
+                  ),
+                ],
+              ),
+            ),
+            _buildClassicComfortsProduct(context),
+            _buildCrispyCravingsProduct(context),
+            _buildFreshBitesProduct(),
+          ],
+        ),
+      ),
+    );
+  }
+}
